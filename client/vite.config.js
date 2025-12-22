c63f60ab6eed8c03f96ae7fd33814ce5f08beda3
0394818020 <datlehuynhhuy@gmail.com>
2025-12-22 20:10:12 +0700

    create dist : index.html

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react],
+  root: path.resolve(__dirname),   // client folder
+  base: './',                      // quan trọng để build relative path
+  build: {
+    outDir: 'dist',                 // folder build
+    emptyOutDir: true,              // xóa dist trước build
+    rollupOptions: {
+      input: path.resolve(__dirname, 'index.html') // bắt buộc phải trỏ tới index.html
+    }
+  }
+});
