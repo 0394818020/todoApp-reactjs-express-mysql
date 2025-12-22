b27bd5a58f7392399abee89a52a9920af2d96143
0394818020 <datlehuynhhuy@gmail.com>
2025-12-22 22:04:02 +0700

    nothing

--- app/server/server.js
+++ app/server/server.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cors({
-    origin: ['http://localhost:3000', 'http://localhost:5173', 'https://todo-api-production.up.railway.app']
+    origin: ['http://localhost:3000', 'http://localhost:5173', 'https://todo-api-production.up.railway.app', 'https://todoapp-reactjs-express-mysql-production.up.railway.app']
 }))
 
 const __filename = fileURLToPath(import.meta.url);
