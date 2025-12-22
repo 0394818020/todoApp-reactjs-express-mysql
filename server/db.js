c8a693685e686ab4a57f02754a5a32c98cedf253
0394818020 <datlehuynhhuy@gmail.com>
2025-12-22 15:51:55 +0700

    again 3

--- server/db.js
+++ server/db.js
@@ -4,24 +4,16 @@ import dotenv from 'dotenv'
 dotenv.config();
 
 export default mysql.createPool({
-  host: process.env.MYSQLHOST,
-  port: process.env.MYSQLPORT,
-  user: process.env.MYSQLUSER,
-  password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE,
+  host: process.env.DATABASE_HOST,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASS,
+  database: process.env.DATABASE_NAME,
+  port: process.env.DATABASE_PORT,
+  ssl: {
+    rejectUnauthorized: false
+  },
   waitForConnections: true,
-  connectionLimit: 10
+  connectionLimit: 10,
 });
 
-// mysql.createPool({
-//   host: process.env.DATABASE_HOST,
-//   user: process.env.DATABASE_USER,
-//   password: process.env.DATABASE_PASS,
-//   database: process.env.DATABASE_NAME,
-//   port: process.env.DATABASE_PORT,
-//   ssl: {
-//     rejectUnauthorized: false
-//   },
-//   waitForConnections: true,
-//   connectionLimit: 10,
-// });
+
